44b66b21c465cd8236518504c4d141746b1f4684
Lalita <hubg1455@gmail.com>
2022-09-11 15:45:54 +0530

    updated logger

--- price_prediction/logger/__init__.py
+++ price_prediction/logger/__init__.py
@@ -7,7 +7,7 @@ LOG_DIR="airfoil_logs"
 
 CURRENT_TIME_STAMP=f"{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}"
 
-LOG_FILE_NAME=f"log_{CURRENT_TIME_STAMP}.logs"
+LOG_FILE_NAME=f"log_{CURRENT_TIME_STAMP}.log"
 
 #If there is no directory then need to create directory
 os.makedirs(LOG_DIR,exist_ok=True)
