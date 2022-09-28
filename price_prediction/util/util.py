b518c5af8cb885cc2933aff0bd4290dfddab769b
Lalita <hubg1455@gmail.com>
2022-09-28 21:13:38 +0530

    updateed

--- price_prediction/util/util.py
+++ price_prediction/util/util.py
@@ -1,6 +1,11 @@
 import yaml
-from price_prediction.exception import PriceException
+
 import os,sys
+import numpy as np
+import dill
+import pandas as pd 
+from price_prediction.constant import *
+from price_prediction.exception import PriceException
 
 
 def read_yaml_file(file_path:str)->dict:
@@ -14,3 +19,86 @@ def read_yaml_file(file_path:str)->dict:
 
     except Exception as e:
         raise PriceException(e,sys) from e
+
+def save_numpy_array_data(file_path: str,array:np.array):
+    """
+    save numpy array data to file
+    file_path: str location of file to save
+    array: np.array data to save
+    
+    """
+    try:
+        dir_path=os.path.dirname(file_path)
+        os.makedirs(dir_path,exist_ok=False)
+        with open(file_path,'wb') as file_obj:
+            np.save(file_obj, array)
+
+    except Exception as e:
+        raise PriceException(e,sys) from e
+
+def load_numpy_array_data(file_path: str)-> np.array:
+
+    """
+        load numpy array data from file
+        file_path: str location of file to load
+        array: np.array data loaded 
+    """   
+    try:
+        with open(file_path,'rb') as file_obj:
+            return np.load(file_obj)
+
+    except Exception as e:
+        raise PriceException(e,sys) from e
+
+def save_object(file_path:str,obj):
+    """
+    file_path:str
+    obj:Any sort of object
+    """
+    try:
+        dir_path=os.path.dirname(file_path)
+        os.makedirs(dir_path,exist_ok=False)
+        with open(file_path,'wb') as file_obj:
+            dill.dump(obj, file_obj)
+
+    except Exception as e:
+        raise PriceException(e,sys) from e
+
+def load_object(file_path:str):
+    """
+    file_path:str
+    
+    """
+    try:
+        with open(file_path,'rb') as file_obj:
+            return dill.load(file_obj)
+
+    except Exception as e:
+        raise PriceException(e,sys) from e
+
+
+
+def load_data(file_path: str, schema_file_path: str)-> pd.DataFrame:
+        try:
+            dataset_schema=read_yaml_file(schema_file_path)
+
+            schema=dataset_schema[DATASET_SCHEMA_COLUMNS_KEY]
+            dataframe= pd.read_csv(file_path)
+
+            error_message= ""
+
+            for column in dataframe.columns:
+                if column in list(schema.keys()):
+                    dataframe[column].astype(schema[column])
+
+                else:
+                    error_message =f"{error_message} \nColumn: [{column}] is not in schema"
+
+                if len(error_message)>0:
+                    raise Exception(error_message)
+                return dataframe
+
+                
+
+        except Exception as e:
+            raise PriceException(e,sys) from e
